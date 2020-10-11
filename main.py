bd664c460156ae21a48350bd89a8bef791aca6cf
Tehnik <andreykryukov14642@gmail.com>
2020-10-11 14:15:49 +0300

    Add CORS

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from fastapi import FastAPI, File, UploadFile
 from typing import List
 from PIL import Image
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
-
-@app.post("/files/")
-async def create_files(files: List[bytes] = File(...)):
-    return {"file_sizes": [len(file) for file in files]}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.post("/uploadfiles/")
 async def create_upload_files(files: List[UploadFile] = File(...)):
-
     image1 = files[0]
     im1 = Image.open(image1.file)
     (width1, height1) = im1.size
     image2 = files[1]
     im2 = Image.open(image2.file)
     (width2, height2) = im2.size
-    return (width1 * height1 + width2 * height2)
+    message = width1 * height1 + width2 * height2
+    return {"message": message}
+
 
